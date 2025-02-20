576163672159725536c5e2887a03d4a57a4fd6f8
A714\1dam <1garsau@gmail.com>
2025-02-20 16:28:59 +0100

    Revert "Actividad dispositivos inteligentes"
    This reverts commit fb242f34a80e480bc79e837634ffe709782a492a.

--- src/main/java/org/example/DispositivosInteligentes/AppDispositivos.java
+++ src/main/java/org/example/DispositivosInteligentes/AppDispositivos.java
@@ -14,10 +14,9 @@ public class AppDispositivos {
         ArrayList<Dispositivo> listaDispositivos = new ArrayList<>();
         listaDispositivos.add(new Televisor("LG SMART"));
         listaDispositivos.add(new ParlanteInteligente("Alexa"));
-        listaDispositivos.add(new AireAcondicionado("Aire Acondicionado"));
+        listaDispositivos.add(new AireAcondicionado("Condicioneiro"));
         listaDispositivos.add(new Televisor("Samsung comedor"));
 
-
         for (Dispositivo dispositivo : listaDispositivos){
             dispositivo.encender();
 
@@ -31,57 +30,7 @@ public class AppDispositivos {
             System.out.println("---------------------------------------");
         }
 
-        Dispositivo proyector = new Dispositivo("Optoma") {
-            @Override
-            public void encender() {
-                if (isEstado()) {
-                    System.out.println("El proyector ya está encendido.");
-                } else {
-                    System.out.println("Encendiendo proyector con ajuste automático de brillo...");
-                    setEstado(true);
-                }
-            }
-        };
-
-        ControlRemoto control_proyector = new ControlRemoto() {
-            @Override
-            public void sincronizar() {
-                System.out.println("Sincronizando proyector con control remoto de presentación...");
-            }
-        };
-
-        Dispositivo horno = new Dispositivo("BALAY HOT") {
-            @Override
-            public void encender() {
-                if(isEstado()){
-                    System.out.println("El horno ya está encendido.");
-                }else {
-                    System.out.println("Calentando horno con ajuste automático de temperatura...");
-                    setEstado(true);
-                }
-            }
-        };
-
-        System.out.println("=========================================");
-
-
-        listaDispositivos.add(horno);
-        listaDispositivos.add(proyector);
-
-        for (Dispositivo dispositivo : listaDispositivos){
-            dispositivo.encender();
-
-            if(dispositivo instanceof ControlRemoto){
-                ((ControlRemoto) dispositivo).sincronizar();
-            } else if (dispositivo.getNombre().equals("Optoma")){
-                control_proyector.sincronizar();
-            }
-
-            dispositivo.mostrarEstado();
-            dispositivo.apagar();
-
-            System.out.println("---------------------------------------");
-        }
 
     }
+
 }
